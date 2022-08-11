2c0e40254114b933275af90f0d726d330fc24fed
Qrowio <66504185+Qrowio@users.noreply.github.com>
2022-08-11 23:13:52 +0100

    Settings update
    Checks if email is already taken. Sanitization 2.0

--- classes/Settings.class.php
+++ classes/Settings.class.php
@@ -14,24 +14,36 @@ class Settings extends Database {
 
             switch($_REQUEST){
                 case !empty($_REQUEST['firstName']):
-                    $this->firstName = filter_var($_REQUEST['firstname'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+                    $this->firstName = filter_var($_REQUEST['firstName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
                 case !empty($_REQUEST['lastName']):
-                    $this->lastName = filter_var($_REQUEST['firstname'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-                case !empty($_REQUEST['email']):
-                    $this->email = filter_var(strtolower($_POST['email']),FILTER_SANITIZE_EMAIL);
+                    $this->lastName = filter_var($_REQUEST['lastName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
                 case !empty($_REQUEST['password']):
-                    $this->password = $_REQUEST['password'];
+                    $this->password = strip_tags($_REQUEST['password']);
                 case !empty($_REQUEST['confirm']):
-                    $this->confirm = $_REQUEST['confirm'];
+                    $this->confirm = strip_tags($_REQUEST['confirm']);
             }
 
-            // Email now updates but services wont. Will fix this later on.
-            $this->statement = $this->connection->prepare("UPDATE users SET firstname = case when '$this->firstName' <> '' then '$this->firstName' else firstname end, lastname = case when '$this->lastName' <> '' then '$this->lastName' else lastname end, email = case when '$this->email' <> '' then '$this->email' else email end WHERE email = :email");
-            $this->statement->execute([':email' => $_SESSION['client']['email']]);
-            if(isset($this->email)){
-                $_SESSION['client']['email'] = $this->email;
+            if(!empty($_REQUEST['email'])){
+                $this->email = filter_var(strtolower($_POST['email']),FILTER_SANITIZE_EMAIL);
+                $this->statement = $this->connection->prepare("SELECT email FROM users WHERE email = :email");
+                $this->statement->execute([':email' => $this->email]);
+                $this->row = $this->statement->fetch(PDO::FETCH_ASSOC);
+                if(isset($this->row['email'])){
+                    // Add better arrow later
+                    echo "Email taken";
+                } else {
+                    $this->statement = $this->connection->prepare("UPDATE users SET firstname = case when '$this->firstName' <> '' then '$this->firstName' else firstname end, lastname = case when '$this->lastName' <> '' then '$this->lastName' else lastname end, email = case when '$this->email' <> '' then '$this->email' else email end WHERE email = :email");
+                    $this->statement->execute([':email' => $_SESSION['client']['email']]);
+                    if(isset($this->email)){
+                        $_SESSION['client']['email'] = $this->email;
+                    }
+                    header("Refresh: 0");
+                }
+            } else {
+                $this->statement = $this->connection->prepare("UPDATE users SET firstname = case when '$this->firstName' <> '' then '$this->firstName' else firstname end, lastname = case when '$this->lastName' <> '' then '$this->lastName' else lastname end WHERE email = :email");
+                $this->statement->execute([':email' => $_SESSION['client']['email']]);
+                header("Refresh: 0");
             }
-            header("Refresh: 0");
         }
     }
 }
